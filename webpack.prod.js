2f416668fd313735e6efc9626da59d8fd76aab02
jProgr <8537691@gmail.com>
2022-12-13 17:27:45 -0600

    Improve JS minimization on production

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,15 @@
 const base = require('./webpack.base.js');
+const TerserPlugin = require("terser-webpack-plugin");
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
   ...base,
   mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin(),
+    ],
+  },
 };
