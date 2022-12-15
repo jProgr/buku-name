a30c1ad7bdecd96e425fd9e11daaa4e651213b25
jProgr <8537691@gmail.com>
2022-12-15 10:31:34 -0600

    Add keyboard shortcuts

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import './styles/main.css';
 import {ElementSearcher} from './search';
 import {Options} from './options';
+import {triggerEvent, removeDots} from './util';
 
 // Options
 const exactMatchElement = document.getElementById('exact-match');
@@ -16,7 +17,18 @@ const searchElement = document.getElementById('search');
 exactMatchElement.addEventListener('change', () => search(searcher, searchElement, options.options));
 searchElement.addEventListener('keyup', () => search(searcher, searchElement, options.options));
 
+// Keyboard shortcuts
+searchElement.addEventListener('keyup', (event) => {
+  if (event.key === '.') searchElement.value = removeDots(searchElement.value);
+});
+window.addEventListener('keyup', (event) => {
+  if (event.key !== '.') return;
+
+  exactMatchElement.checked = !exactMatchElement.checked;
+  triggerEvent(exactMatchElement, 'change');
+});
+
 function search(searcher, searchElement, options) {
   const query = searchElement.value.trim().toLowerCase();
-  searcher.search(query, options);
+  searcher.search(removeDots(query), options);
 }
--- src/util.js
+++ src/util.js
@@ -7,3 +7,13 @@ export function stringToBool(value) {
     default: return !!value;
   }
 }
+
+export function triggerEvent(element, eventName) {
+  const event = document.createEvent("HTMLEvents");
+  event.initEvent(eventName, false, true);
+  element.dispatchEvent(event);
+}
+
+export function removeDots(string) {
+  return string.replace(/\.+/, '');
+}
