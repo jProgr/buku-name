5ae8c17411425934038979131a7971fedb6ee04f
jProgr <8537691@gmail.com>
2023-03-11 10:33:51 -0600

    Refactor words into new word class

---
+++ src/word.js
@@ -0,0 +1,82 @@
+/**
+ * Enum for word types.
+ *
+ * @readonly
+ * @enum {Symbol}
+ */
+export const WordType = Object.freeze({
+  BASE: Symbol('base'),
+  COMPOUND: Symbol('compound'),
+});
+
+export class Word {
+
+  /**
+   * Creates a Word.
+   *
+   * @param {HTMLElement} element
+   */
+  constructor(element) {
+    this._element = element;
+    this.word = element.id;
+    this.contents = this._getContent(element.innerText);
+    this.type = this._getType(this.word);
+  }
+
+  /**
+   * Returns the type of word by looking at the string.
+   *
+   * @param {string} word
+   * @return {WordType}
+   */
+  _getType(word) {
+    if (word.includes('-')) return WordType.COMPOUND;
+
+    return WordType.BASE;
+  }
+
+  /**
+   * Returns a set with the word and its meanings.
+   *
+   * @param {string} string
+   * @return {Set} Of strings.
+   */
+  _getContent(string) {
+    let contents = new Set();
+
+    string
+      .split(/[^\w-]/)
+      .forEach(word => contents.add(word));
+    if (contents.has('')) contents.delete('');
+
+    return contents;
+  }
+
+  /**
+   * Marks element as visible.
+   *
+   * @return {undefined}
+   */
+  show() {
+    this._element.style.display = 'block';
+  }
+
+  /**
+   * Marks element as invisible.
+   *
+   * @return {undefined}
+   */
+  hide() {
+    this._element.style.display = 'none';
+  }
+
+  /**
+   * Whether this word is of any of the listed types.
+   *
+   * @param  {...WordType} types
+   * @return {boolean}
+   */
+  is(...types) {
+    return types.includes(this.type);
+  }
+}
